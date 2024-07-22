0943f997af698e3525043434a5728f8afc27625e
wjy1122 <123622051+wjy1122@users.noreply.github.com>
2024-07-22 21:31:58 +0800

    Add files via upload

---
+++ Day-02/2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        int cnt[26]{};
+        // 统计字符频率
+        for (char b : s) {
+            cnt[b - 'a']++;
+        }
+
+        int ans = 0;
+        // 计算最小长度
+        for (int c : cnt) {
+            ans += (c - 1) % 2 + 1;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "abacb";
+    cout << "Minimum length for \"" << s << "\" is: " << solution.minimumLength(s) << endl;
+
+    return 0;
+}
