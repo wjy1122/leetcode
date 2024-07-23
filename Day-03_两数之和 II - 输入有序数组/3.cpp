be41cba4053068e30ab97ebc7716d38f7c478128
wjy1122 <123622051+wjy1122@users.noreply.github.com>
2024-07-23 20:47:53 +0800

    Add files via upload

---
+++ Day-03_两数之和 II - 输入有序数组/3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+        while (true) {
+            int s = numbers[left] + numbers[right];
+            if (s == target) return { left + 1, right + 1 }; // 题目要求下标从 1 开始
+            s > target ? --right : ++left;
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> numbers = { 2, 7, 11, 15 };
+    int target = 9;
+    vector<int> result = solution.twoSum(numbers, target);
+    cout << "Indexes: " << result[0] << ", " << result[1] << endl;
+    return 0;
+}
